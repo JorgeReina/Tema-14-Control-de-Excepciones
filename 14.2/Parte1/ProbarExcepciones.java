d7b25a0f4620c71ff28a02b1752fbf40d1c734dc
Jorge Reina <reinaromerojorge@gmail.com>
2023-04-28 12:59:20 +0200

    Parte 1

--- 14.2/Parte1/ProbarExcepciones.java
+++ 14.2/Parte1/ProbarExcepciones.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ProbarExcepciones {
@@ -18,11 +22,11 @@ public class ProbarExcepciones {
         System.out.println("Opcion 5: ArithmeticException");
         System.out.println("Opcion 6: Salir");
         System.out.println();
-        System.out.print("Introduce una opción: ");
+        System.out.print("Introduce una opcion: ");
         int opcion = datos.nextInt();
         switch (opcion) {
           case 1:
-              
+              opcion1(0);
             break;
           case 2:
               opcion2();
@@ -31,7 +35,7 @@ public class ProbarExcepciones {
               opcion3();
             break;
           case 4:
-              
+              opcion4();
             break;
           case 5:
               opcion5(18);
@@ -48,6 +52,17 @@ public class ProbarExcepciones {
     }
 
   }
+
+  public static void opcion1(int i) throws StackOverflowError {
+    try {
+      System.out.println("Llamada número: " + i);
+      opcion1(i+1);
+    } catch (StackOverflowError e) {
+      System.out.println(e.getMessage());
+      System.out.println(e.getClass());
+      throw e;
+    }
+}
 
   public static void opcion2() throws NumberFormatException {
     
@@ -72,6 +87,22 @@ public class ProbarExcepciones {
       System.out.println(e.getClass());
       throw e;
     }
+  }
+
+  public static void opcion4() {
+    try {
+        BufferedReader reader = new BufferedReader(new FileReader("archivo_que_no_existe.txt"));
+        String linea = null;
+        while ((linea = reader.readLine()) != null) {
+            System.out.println(linea);
+        }
+      } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+      } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+      }
   }
 
   public static int opcion5(int x) throws ArithmeticException {
